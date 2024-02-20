7f631175b36cababcf70ed737626217fa4fa833e
Mahfuja <bajibaji1004@gmail.com>
2024-02-20 19:35:42 +0600

    updated again, 10 requirements done

--- app/src/main/java/com/example/myfirstapp/AboutActivity.java
+++ app/src/main/java/com/example/myfirstapp/AboutActivity.java
@@ -17,10 +17,14 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         webview=(WebView) findViewById(R.id.webvideo);
-        String video="<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/yp58geKmdYE?si=fhdLEU91ZtjKNJ4v\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>";
-        webview.loadData(video,"text/html","utf-8");
+
+        String htmlContent = "<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"file:///android_res/raw/styles.css\" /></head><body><iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/yp58geKmdYE?si=fhdLEU91ZtjKNJ4v\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></body></html>";
+       // String video="<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/yp58geKmdYE?si=fhdLEU91ZtjKNJ4v\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>";
+        webview.loadDataWithBaseURL(null,htmlContent,"text/html","utf-8",null);
+
         webview.getSettings().setJavaScriptEnabled(true);
         webview.setWebChromeClient(new WebChromeClient());
+
 
         Fragment fragment= new Map_Fragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.location,fragment).commit();
